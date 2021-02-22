fb17c91ab6bbadb5f80e7643c6d596fdfbaa65e1
Devon <onlycloudy112042@gmail.com>
2021-02-22 11:05:18 +0800

    Update admin.py

--- checknews/admin.py
+++ checknews/admin.py
@@ -4,3 +4,4 @@ from checknews.models import authCommittee,authAssistant
 # Register your models here.
 admin.site.register(authCommittee)
 admin.site.register(authAssistant)
+admin.site.register(postBlog)
